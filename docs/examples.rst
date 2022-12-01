81d08d72d781ed4c832ca9a536c6b84c0dd07815
Peter Weir <pweir@squareup.com>
2022-11-30 17:33:32 -0800

    added nicer error patch example using data from sql query

--- docs/examples.rst
+++ docs/examples.rst
@@ -12,6 +12,11 @@ First example
 Displaying error patches
 ------------------------
 
+The following demonstrates how to display error patches with output from a
+typical SQL query. (You can see the query that generated the data using an open
+data set available through the `Bigquery`_ console.)
+
+.. _Bigquery: https://console.cloud.google.com/marketplace/product/sportradar-public-data/mlb-pitch-by-pitch
 .. literalinclude:: ../examples/error_patch_example.py
 
 ..  figure:: ../examples/error_patch_example.svg
--- examples/error_patch_example.py
+++ examples/error_patch_example.py
@@ -1,28 +1,38 @@
 import datura
+import pandas as pd
 
-labels = ['line #1', 'line #2', 'line #3']
-label_nudges = [0, -2, 5]
+sql_query = """
+WITH final_score_CTE AS (
+SELECT 
+    MIN(homeFinalRuns) AS home_runs
+  , MIN(awayFinalRuns) AS away_runs
+  , MIN(startTime) AS start_time
+  , gameID
+FROM `bigquery-public-data.baseball.games_wide`
+WHERE gameStatus = 'closed'
+GROUP BY gameID
+)
+SELECT
+  AVG(home_runs) AS avg_home_runs
+  , STDDEV_SAMP(home_runs) AS std_home_runs
+  , AVG(away_runs) AS avg_away_runs
+  , STDDEV_SAMP(away_runs) AS std_away_runs
+  , COUNT( DISTINCT gameID) AS games
+  , DATE_TRUNC(start_time, MONTH) AS game_month
+FROM final_score_CTE
+GROUP BY game_month
+ORDER BY game_month
+"""
 
-x1 = [xx*.1 for xx in range(-100, 101)]
-xs = [x1, x1, x1]
+results_df = pd.read_csv('examples/runs.csv', parse_dates=['game_month'])
+results_df['sem_home'] = results_df['std_home_runs'] / results_df['games']**.5
+results_df['sem_away'] = results_df['std_away_runs'] / results_df['games']**.5
 
-y1 = [(xx*.035)**3 for xx in range(-100, 101)]
-y2 = [(xx*.1)**2 for xx in range(-100, 101)]
-y3 = [int(str(xx**10)[:1]) + int(xx > 0)*(xx*.1)**2 for xx in range(-100, 101)]
-ys = [y1, y2, y3]
-
-yu1 = [y+10 for y in y1]
-yl1 = [y-10 for y in y1]
-yu2 = y2  # this is a bit of a hack
-yl2 = y2
-yu3 = [y*1.3+5 for y in y3]
-yl3 = [y*.5-10 for y in y3]
-yus = [yu1, yu2, yu3]
-yls = [yl1, yl2, yl3]
-
-datura.error_plot(xs, ys, yus=yus, yls=yls,
+datura.error_plot(results_df['game_month'],
+                  results_df[['avg_home_runs', 'avg_away_runs']],
+                  y_errors=results_df[['sem_home', 'sem_away']],
                   filename='examples/error_patch_example.svg',
-                  x_label='x label', y_label='y label',
-                  title='Error Patch Example', labels=labels,
-                  label_nudges=label_nudges, x_ticks=[-5, -10, 0, 5, 10],
-                  y_ticks=[-50, 0, 50, 100])
+                  x_label='Game month', y_label='Runs (+/- SE)',
+                  title='Error Patch Example', labels=['Home', 'Away'],
+                  label_nudges=[0, 0],
+                  y_ticks=[0, 2, 4, 6])
