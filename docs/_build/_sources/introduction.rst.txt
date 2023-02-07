001907d3943be6f758f6603f7cccb9325e38b55e
Peter Weir <pweir@squareup.com>
2023-02-07 14:01:20 -0800

    testing ticks

--- docs/_build/_sources/introduction.rst.txt
+++ docs/_build/_sources/introduction.rst.txt
@@ -23,12 +23,12 @@ So why create another? Datura is free and open source, easy to install, and work
 Further, Datura creates figures that are beautiful by default and reproducible.
 
 Each one of these criteria resulted in design choices for Datura that make it substantially different from other libraries.
-Insisting that it be easy to install resulted in the choice to have zero requirements (outside of the Python standard library).
+Insisting that it be easy to install resulted in the choice to have zero requirements outside of the Python standard library.
 Datura does not have any configuration files that users need to maintain or adjust (more on this later).
 
 The desire to have it work in a variety of environments drove the usage of svg files as the file format — these files can be rendered anywhere a notebook is running and basically any environment with a browser.
 
-Although beauty is subjective, the default settings for Datura are intentionally chosen to take advantage of the space and colors available to the human visual system to express common quantitative concepts clearly and with minimal clutter.
+Although beauty is subjective, the default settings for Datura are intentionally chosen to express common quantitative concepts clearly and with minimal clutter.
 
 The final motivating factor, reproducibility, drove the decision to wrap all figure creation into a single line of code.
 While this design can result in commands with somewhat lengthy lists of arguments, it guarantees that running the same command generates the same figure, which is especially important in notebook environments where individual cells can be run and re-run in any order.
--- tests/test_make_pretty_ticks.py
+++ tests/test_make_pretty_ticks.py
@@ -1,4 +1,8 @@
 import datura
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import os
 
 
 def test_num2pretty_string():
@@ -7,3 +11,71 @@ def test_num2pretty_string():
     for in_out in in_outs:
         _out = datura.draw._num2pretty_string(in_out[0])
         assert _out == in_out[1]
+
+
+def test_year_ticks():
+    time_range = pd.date_range(start='2010-01-01', end='2020-01-01', freq='YS')
+    df = pd.DataFrame(data={'t': time_range,
+                            'y1': np.sin(np.linspace(0, 2*np.pi, 11)),
+                            'y2': np.cos(np.linspace(0, 4*np.pi, 11))})
+
+    this_fn_base = 'year_ticks'
+    out_svg = os.path.join('tests', this_fn_base+'.svg')
+    persistent_svg = os.path.join('tests', this_fn_base+'_persistent.svg')
+
+    _out = datura.plot(df['t'], df[['y1', 'y2']], filename=out_svg)
+
+    with open(out_svg) as out_svg_file:
+        with open(persistent_svg) as persistent_svg_file:
+            assert out_svg_file.read() == persistent_svg_file.read()
+
+
+def test_month_ticks():
+    time_range = pd.date_range(start='2023-01-01', end='2024-01-01', freq='MS')
+    df = pd.DataFrame(data={'t': time_range,
+                            'y1': np.sin(np.linspace(0, 2*np.pi, 13)),
+                            'y2': np.cos(np.linspace(0, 4*np.pi, 13))})
+
+    this_fn_base = 'month_ticks'
+    out_svg = os.path.join('tests', this_fn_base+'.svg')
+    persistent_svg = os.path.join('tests', this_fn_base+'_persistent.svg')
+
+    _out = datura.plot(df['t'], df[['y1', 'y2']], filename=out_svg)
+
+    with open(out_svg) as out_svg_file:
+        with open(persistent_svg) as persistent_svg_file:
+            assert out_svg_file.read() == persistent_svg_file.read()
+
+
+def test_week_ticks():
+    time_range = pd.date_range(start="2023-01-01", end="2024-01-01", freq="W")
+    df = pd.DataFrame(data={'t': time_range,
+                            'y1': np.sin(np.linspace(0, 2*np.pi, 53)),
+                            'y2': np.cos(np.linspace(0, 4*np.pi, 53))})
+
+    this_fn_base = 'week_ticks'
+    out_svg = os.path.join('tests', this_fn_base+'.svg')
+    persistent_svg = os.path.join('tests', this_fn_base+'_persistent.svg')
+
+    _out = datura.plot(df['t'], df[['y1', 'y2']], filename=out_svg)
+
+    with open(out_svg) as out_svg_file:
+        with open(persistent_svg) as persistent_svg_file:
+            assert out_svg_file.read() == persistent_svg_file.read()
+
+
+def test_day_ticks():
+    time_range = pd.date_range(start="2023-08-01", end="2023-09-01", freq="D")
+    df = pd.DataFrame(data={'t': time_range,
+                            'y1': np.sin(np.linspace(0, 2*np.pi, 32)),
+                            'y2': np.cos(np.linspace(0, 4*np.pi, 32))})
+
+    this_fn_base = 'day_ticks'
+    out_svg = os.path.join('tests', this_fn_base+'.svg')
+    persistent_svg = os.path.join('tests', this_fn_base+'_persistent.svg')
+
+    _out = datura.plot(df['t'], df[['y1', 'y2']], filename=out_svg)
+
+    with open(out_svg) as out_svg_file:
+        with open(persistent_svg) as persistent_svg_file:
+            assert out_svg_file.read() == persistent_svg_file.read()
